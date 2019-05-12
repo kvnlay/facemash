c19238a629e97bf9f962588b9a172a3dc4e17330
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-05-12 18:39:14 +0300

    add comment validation tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
-  validates :user, presence: true
-  validates :post, presence: true
-  
   belongs_to :user
   belongs_to :post
+
+  validates :content, presence: true, length: { maximum: 60 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:musa)
+    @post = posts(:sample_post)
+    @comment = Comment.new(content: 'Lorem ipsum', user_id: @user.id,\
+                           post_id: @post.id)
+  end
+
+  test 'should be valid' do
+    assert @comment.valid?
+  end
+
+  test 'user id should be present' do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+
+  test 'post id should be present' do
+    @comment.post_id = nil
+    assert_not @comment.valid?
+  end
+
+  test 'content should be present' do
+    @comment.content = '   '
+    assert_not @comment.valid?
+  end
+
+  test 'content should be at most 60 characters' do
+    @comment.content = 'a' * 61
+    assert_not @comment.valid?
+  end
 end
