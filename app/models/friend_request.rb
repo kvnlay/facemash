501bab42ff45dc8ef1bbd986cf34be260251b6f0
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-05-12 21:06:22 +0300

    add friend request validation tests

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,7 @@
 class FriendRequest < ApplicationRecord
   belongs_to :requester, class_name: 'User'
   belongs_to :requested, class_name: 'User'
+
+  validates :requester, presence: true
+  validates :requested, presence: true
 end
--- test/models/friend_request_test.rb
+++ test/models/friend_request_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class FriendRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @requester = users(:musa)
+    @requested = users(:kingsley)
+    @friend_request = FriendRequest.new(requester_id: @requester.id,\
+                                        requested_id: @requested.id)
+  end
+
+  test 'should be valid' do
+    assert @friend_request.valid?
+  end
+
+  test 'requester id should be present' do
+    @friend_request.requester = nil
+    assert_not @friend_request.valid?
+  end
+
+  test 'requested id should be present' do
+    @friend_request.requested = nil
+    assert_not @friend_request.valid?
+  end
 end
