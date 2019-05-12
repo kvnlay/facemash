d951e5c9a0c7c2246a0564feda290ac9fcd4b376
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-05-12 20:10:57 +0300

    add friendship validation tests

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:musa)
+    @friend = users(:kingsley)
+    @friendship = Friendship.new(user_id: @user.id, friend_id: @friend.id)
+  end
+
+  test 'should be valid' do
+    assert @friendship.valid?
+  end
+
+  test 'user id should be present' do
+    @friendship.user_id = nil
+    assert_not @friendship.valid?
+  end
+
+  test 'friend id should be present' do
+    @friendship.friend_id = nil
+    assert_not @friendship.valid?
+  end
 end
