78fa10ddc261da82a75ed68283527b22c63f4bc2
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-05-12 17:06:42 +0300

    add post validation tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
-validates :user, presence: true
-validates :body, presence: true, length: { maximum: 200 }
-
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  validates :user_id, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:musa)
+    @post = Post.new(body: 'Lorem ipsum', user_id: @user.id)
+  end
+
+  test 'should be valid' do
+    assert @post.valid?
+  end
+
+  test 'user id should be present' do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test 'body should be present' do
+    @post.body = '   '
+    assert_not @post.valid?
+  end
+
+  test 'body should be at most 140 characters' do
+    @post.body = 'a' * 201
+    assert_not @post.valid?
+  end
 end
