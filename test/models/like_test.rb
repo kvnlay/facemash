2648cb534e2464ab63c9927e7af767ebe3e9bd87
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-05-12 18:45:06 +0300

    add like validation tests

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:musa)
+    @post = posts(:sample_post)
+    @like = Like.new(user_id: @user.id, post_id: @post.id)
+  end
+
+  test 'should be valid' do
+    assert @like.valid?
+  end
+
+  test 'user id should be present' do
+    @like.user_id = nil
+    assert_not @like.valid?
+  end
+
+  test 'post id should be present' do
+    @like.post_id = nil
+    assert_not @like.valid?
+  end
 end
