4557c1a5c89338d406618dcd1df6157535bfeecb
kvnlay <kingstevens007@gmail.com>
2019-05-16 14:52:43 +0100

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
+  root to: 'users#index'
   get 'friends/index'
   get 'friends/destroy'
   get 'friend_requests/index'
@@ -6,5 +9,6 @@ Rails.application.routes.draw do
   get 'friend_requests/update'
   get 'friend_requests/destroy'
   devise_for :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
