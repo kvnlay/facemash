cc45de30d74622a23b1eceb77462dc1644bc8fcb
kvnlay <kingstevens007@gmail.com>
2019-05-29 13:04:52 +0100

    remove tests folder

--- test/models/friend_request_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class FriendRequestTest < ActiveSupport::TestCase
-  should belong_to(:requester)
-    .class_name('User')
-  should belong_to(:requested)
-    .class_name('User')
-end
--- test/models/like_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-class LikeTest < ActiveSupport::TestCase
-  should belong_to(:user)
-  should belong_to(:post)
-end
