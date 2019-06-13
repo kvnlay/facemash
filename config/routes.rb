5b0f66f9ee7cea1a11eb7a6ede9a748072a7c6ad
kvnlay <kingstevens007@gmail.com>
2019-06-13 09:53:02 +0100

    Add privacy policy page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:new, :show, :create, :destroy]
   end
+  get 'privacy_policy', to: 'welcome#policy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
